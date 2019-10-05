1d1f22cf228f5c148f7bdb9217ce7d9210be306b
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:28:51 +0200

    initial commit: index.html and test.js - simple js chchecks if youre 18 to 30 yo to go into da club

---
+++ test.js
@@ -0,0 +1,19 @@
+var myAge = 25;
+
+if (myAge >=18 && myAge <= 30) {  // && I //
+    document.write("You can come in");
+    
+} else {
+    document.write ("you aint coming");
+
+}
+
+var myAge = 25;
+
+if (myAge < 18 || myAge > 30) {     // || LUB //
+    document.write("You can come in");
+    
+} else {
+    document.write ("you aint coming");
+
+}
